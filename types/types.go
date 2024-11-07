d8cbd7365dadf0ecea5e9b90ac2bff71d2c2bbe9
Ifeanyichukwu Ifeanyichukwu Hillary <98ifeanyichukwu@gmail.com>
2024-11-08 00:51:13 +0100

    add file types and methods to use with app

---
+++ types/types.go
@@ -0,0 +1,29 @@
+// specifies the data types needed for the application
+package types
+
+import "fmt"
+
+type Task struct {
+	Id          int    `json:"id"`
+	Description string `json:"description"`
+	Status      string `json:"status"`
+	CreatedAt   string `json:"createdAt"`
+	UpdatedAt   string `json:"updatedAt"`
+}
+
+type Tasks []Task
+
+func (t Tasks) Output() {
+	var task Task
+
+	if len(t) == 1 {
+		task = t[0]
+		fmt.Printf("Task %v, added successfully (ID: %v)\n", task.Description, task.Id)
+	} else {
+		for _, task = range t {
+			fmt.Printf("Task %v, added successfully (ID: %v)\n", task.Description, task.Id)
+		}
+	}
+}
+
+const JsonFile string = "tasks.json"
