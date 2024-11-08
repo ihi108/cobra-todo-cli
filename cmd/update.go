91ea2f312f9a30f6da2344cc228732641b700bde
Ifeanyichukwu Ifeanyichukwu Hillary <98ifeanyichukwu@gmail.com>
2024-11-08 16:32:10 +0100

    handle update command for updating tasks

--- cmd/update.go
+++ cmd/update.go
@@ -4,9 +4,14 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"os"
 	"fmt"
+	"log"
+	"strconv"
 
 	"github.com/spf13/cobra"
+	appDefs "github.com/ihi108/cobra-todo-cli/types"
+	utils "github.com/ihi108/cobra-todo-cli/utils"
 )
 
 // updateCmd represents the update command
@@ -18,7 +23,45 @@ var updateCmd = &cobra.Command{
 	task-cli udpate 1 "Buy groceries and cook dinner"
 `,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("update called")
+		var tasks appDefs.Tasks
+		var newTasks appDefs.Tasks
+		var found bool
+
+		tasks = make(appDefs.Tasks, 0)
+		newTasks = make(appDefs.Tasks, 0)
+		found = false
+
+		if len(args) < 2 {
+			str := fmt.Sprintf("Usage:\n  todo-cli update [taskID] [taskDescription]")
+			fmt.Println(str)
+			os.Exit(1)
+		}
+
+		id, task := args[0], args[1]
+
+		bytes := utils.ReadFile(appDefs.JsonFile)
+		utils.UnmarshalJSON(bytes, &tasks)
+		idInt, err := strconv.Atoi(id)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// update task
+		for _, item := range tasks {
+			if item.Id == idInt {
+				item.Description = task
+				found = true
+			}
+			newTasks = append(newTasks, item)
+		}
+		if found == false {
+			msg := fmt.Sprintf("Task with ID: %v, Not Found\n", id)
+			log.Fatal(msg)
+		}
+
+		bytes = utils.MarshalJSON(newTasks)
+		utils.WriteFile(appDefs.JsonFile, bytes)
+		tasks.UpdateOutput(idInt)
 	},
 }
 
